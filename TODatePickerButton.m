3f2bc01e65059635efc45e52fbdb680f359465f8
tousan <790847823@qq.com>
2016-08-18 17:01:45 +0800

    change default style

--- TODatePickerButton.m
+++ TODatePickerButton.m
@@ -32,6 +32,7 @@
         _datePicker.datePickerMode = UIDatePickerModeDate;
         [_datePicker addTarget:self action:@selector(datePickerChanged) forControlEvents:UIControlEventValueChanged];
         _bar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-250, [UIScreen mainScreen].bounds.size.width, 34)];
+        _bar.barStyle = UIBarStyleBlack;
         UIBarButtonItem *cancel_Btn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismiss)];
         _bar.items = @[cancel_Btn];
         [[[UIApplication sharedApplication].delegate window]addSubview:_datePicker];
@@ -63,9 +64,26 @@ static const char scrollViewKey = '\02';
     [[[UIApplication sharedApplication].delegate window]sendSubviewToBack:_datePicker];
     [[[UIApplication sharedApplication].delegate window]sendSubviewToBack:_bar];
     [[[UIApplication sharedApplication].delegate window]bringSubviewToFront:[(UIViewController*)_viewController view]];
-    [UIView animateWithDuration:0.3 animations:^{
-        [(UIViewController*)_viewController view].frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
-    }];
+    if (_scrollView)
+    {
+        CGFloat contentHeight = [objc_getAssociatedObject(_viewController, &contentHeightKey) floatValue];
+        _scrollView.contentSize = CGSizeMake(_scrollView.contentSize.width, contentHeight);
+        objc_setAssociatedObject(_scrollView, &isContentChanged, @(NO), OBJC_ASSOCIATION_RETAIN);
+        if (_scrollView.contentOffset.y>(_scrollView.contentSize.height-_scrollView.frame.size.height))
+        {
+            if (_scrollView.contentSize.height-_scrollView.frame.size.height>0)
+            {
+                [_scrollView setContentOffset:CGPointMake(0, _scrollView.contentSize.height-_scrollView.frame.size.height) animated:YES];
+            }
+        }
+    }
+    else
+    {
+        [UIView animateWithDuration:0.3 animations:^{
+            UIView *superView = [(UIViewController*)_viewController view];
+            superView.frame = CGRectMake(superView.frame.origin.x, 0, superView.frame.size.width, superView.frame.size.height);
+        }];
+    }
 }
 
 static const char isContentChanged = '\03';
@@ -84,7 +102,7 @@ static const char isContentChanged = '\03';
     CGRect rect = [[(UIViewController*)_viewController view] convertRect:self.frame fromView:self.superview];
     CGRect inrect = [_scrollView convertRect:self.frame fromView:self.superview];
     CGFloat bottom = rect.origin.y+rect.size.height;
-    CGFloat top = [UIScreen mainScreen].bounds.size.height-260;
+    CGFloat top = [UIScreen mainScreen].bounds.size.height-283;
     
     if (_scrollView)
     {
@@ -92,7 +110,7 @@ static const char isContentChanged = '\03';
         if (!isChanged)
         {
             CGFloat contentHeight = [objc_getAssociatedObject(_viewController, &contentHeightKey) floatValue];
-            _scrollView.contentSize = CGSizeMake(_scrollView.contentSize.width, contentHeight+250);
+            _scrollView.contentSize = CGSizeMake(_scrollView.contentSize.width, contentHeight+283);
             objc_setAssociatedObject(_scrollView, &isContentChanged, @(YES), OBJC_ASSOCIATION_RETAIN);
         }
         if (bottom<top)
@@ -100,7 +118,7 @@ static const char isContentChanged = '\03';
             return;
         }
         [UIView animateWithDuration:0.3 animations:^{
-            [_scrollView setContentOffset:CGPointMake(0, inrect.origin.y-20)];
+            [_scrollView setContentOffset:CGPointMake(0, inrect.origin.y-[UIScreen mainScreen].bounds.size.height+283)];
         }];
     }
     else
